dc4c1ea004f2240340737ca73b51afc508c921f5
ALonelyPsyduck <wyzhi02@gmail.com>
2023-01-27 23:12:34 +0800

    Cap the FPS at 165 maximum

--- Assets/Code/Scripts/ResolutionManager.cs
+++ Assets/Code/Scripts/ResolutionManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,7 +12,12 @@ public class ResolutionManager : MonoBehaviour
 
     private float currentRefreshRate;
     private int currentResolutionIndex = 0;
-    
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 165;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
